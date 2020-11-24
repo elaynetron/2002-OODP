c59f15668636a220d4d655c47f15e69e7ecba6fb
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 23:55:43 +0800

    Update CourseMgr.java

--- codes/src/control/CourseMgr.java
+++ codes/src/control/CourseMgr.java
@@ -1,62 +1,59 @@
 package control;
 
+import java.io.*;
+import java.util.*;
+
 import entity.Course;
 import entity.Index;
-
-import java.io.EOFException;
-import java.io.IOException;
-import java.util.List;
+import entity.Student;
 
 public class CourseMgr {
-    protected int getWaitListStudent(String courseCode, int indexNumber) throws EOFException, ClassNotFoundException, IOException {
-        Index indexNum = filterCourseByCourseCodeAndIndexNum(courseCode, indexNumber);
-        if (indexNum == null) {
-            return -1;
-        }
+	protected Student getWaitListStudent(String courseCode, int indexNumber)
+			throws EOFException, ClassNotFoundException, IOException {
+		Index index = filterCourseByCourseCodeAndIndexNum(courseCode, indexNumber);
+		return index.getWaitList().get(0);
+	}
 
-        if (indexNum.getWaitList() == null) {
-            return 0;
-        }
+	public int checkVacancies(String courseCode, int indexNumber)
+			throws EOFException, ClassNotFoundException, IOException {
+		Index indexNum = filterCourseByCourseCodeAndIndexNum(courseCode, indexNumber);
+		if (indexNum == null) {
+			return -1;
+		}
+		return indexNum.getVacancy();
+	}
 
-        return indexNum.getWaitList().size();
-    }
+	public boolean validateCourse(String courseCode) throws EOFException, ClassNotFoundException, IOException {
+		List<Course> courseList = DataMgr.readCourseList();
+		Course filteredCourse = courseList.stream()
+				.filter(course -> course.getCourseCode().equalsIgnoreCase(courseCode)).findAny().orElse(null);
+		if (filteredCourse == null) {
+			return false;
+		}
+		return true;
+	}
 
-    public int checkVacancies(String courseCode, int indexNumber) throws EOFException, ClassNotFoundException, IOException {
-        Index indexNum = filterCourseByCourseCodeAndIndexNum(courseCode, indexNumber);
-        if (indexNum == null) {
-            return -1;
-        }
-        return indexNum.getVacancy();
-    }
-    
-    public boolean validateCourse(String courseCode) throws EOFException, ClassNotFoundException, IOException {
-        List<Course> courseList = DataMgr.readCourseList();
-        Course filteredCourse =  courseList.stream().filter(course -> course.getCourseCode().equalsIgnoreCase(courseCode)).findAny().orElse(null);
-        if (filteredCourse == null) {
-            return false;
-        }
-        return true;
-    }
-    
-    public Course getCourse(String courseCode) throws EOFException, ClassNotFoundException, IOException {
-    	List<Course> courseList = DataMgr.readCourseList();
-    	for (Course course: courseList) {
-    		if (course.getCourseCode().compareTo(courseCode) == 0) return course;
-    	}
-    	return null;
-    }
+	public Course getCourse(String courseCode) throws EOFException, ClassNotFoundException, IOException {
+		List<Course> courseList = DataMgr.readCourseList();
+		for (Course course : courseList) {
+			if (course.getCourseCode().compareTo(courseCode) == 0)
+				return course;
+		}
+		return null;
+	}
 
-    public boolean validateIndexNum(String courseCode, int indexNumber) throws EOFException, ClassNotFoundException, IOException {
-        Index indexNum = filterCourseByCourseCodeAndIndexNum(courseCode, indexNumber);
-        if (indexNum == null) {
-            return false;
-        }
-        return true;
-    }
-    
-    public Index getIndex(String courseCode, int indexNum) throws EOFException, ClassNotFoundException, IOException {
+	public boolean validateIndexNum(String courseCode, int indexNumber)
+			throws EOFException, ClassNotFoundException, IOException {
+		Index indexNum = filterCourseByCourseCodeAndIndexNum(courseCode, indexNumber);
+		if (indexNum == null) {
+			return false;
+		}
+		return true;
+	}
+
+	public Index getIndex(String courseCode, int indexNum) throws EOFException, ClassNotFoundException, IOException {
 		Course course = getCourse(courseCode);
-		for (Index index: course.getIndexList()) {
+		for (Index index : course.getIndexList()) {
 			if (index.getIndexNum() == indexNum) {
 				return index;
 			}
@@ -64,11 +61,11 @@ public class CourseMgr {
 		return null;
 	}
 
-    private Index filterCourseByCourseCodeAndIndexNum(String courseCode, int indexNumber) throws EOFException, ClassNotFoundException, IOException {
-        List<Course> courseList = DataMgr.readCourseList();
-        return courseList.stream().filter(course -> course.getCourseCode().equalsIgnoreCase(courseCode))
-            .flatMap(course -> course.getIndexList().stream())
-            .filter(indexNum -> indexNumber == indexNum.getIndexNum())
-            .findAny().orElse(null);
-    }
+	private Index filterCourseByCourseCodeAndIndexNum(String courseCode, int indexNumber)
+			throws EOFException, ClassNotFoundException, IOException {
+		List<Course> courseList = DataMgr.readCourseList();
+		return courseList.stream().filter(course -> course.getCourseCode().equalsIgnoreCase(courseCode))
+				.flatMap(course -> course.getIndexList().stream())
+				.filter(indexNum -> indexNumber == indexNum.getIndexNum()).findAny().orElse(null);
+	}
 }
