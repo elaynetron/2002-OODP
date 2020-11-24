d97127e301d48e3412fb9c1c83972ce154c75544
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 23:54:45 +0800

    Update MainUI.java

--- codes/src/boundary/MainUI.java
+++ codes/src/boundary/MainUI.java
@@ -1,20 +1,18 @@
 package boundary;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
-import java.util.Scanner;
+import java.util.*;
 
 public class MainUI {
 	private static Scanner sc;
-	
+
 	public static void main(String[] args) throws IOException, ParseException {
 		sc = new Scanner(System.in);
 		int choice = 0;
 		// Can add MainUI functions other than login for future extensions here
 		do {
-			System.out.println("Welcome to STARS Planner!\n"
-					+ "Please select:\n"
-					+ "[1] Login as Admin/Student\n"
+			System.out.println("Welcome to STARS Planner!\n" + "Please select:\n" + "[1] Login as Admin/Student\n"
 					+ "[2] Quit Application");
 			try {
 				choice = Integer.parseInt(sc.nextLine());
@@ -22,22 +20,21 @@ public class MainUI {
 				case 1: // Login
 					new LoginUI(sc);
 					break;
-					
+
 				case 2:
 					System.out.println("Quitting Application...\n");
 					break;
-					
+
 				default:
 					System.out.println("Invalid, please try again.\n");
 				}
 			}
-			
+
 			catch (Exception e) {
 				System.out.println("Invalid, please try again.\n");
 			}
 		} while (choice != 2);
-		
-		
+
 	}
 
 }
