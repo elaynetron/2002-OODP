436410bc9bc5be71d595a87e61f89ecb17ded36d
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 23:59:31 +0800

    Update RegisteredCourse.java

--- codes/src/entity/RegisteredCourse.java
+++ codes/src/entity/RegisteredCourse.java
@@ -1,43 +1,47 @@
 package entity;
 
 public class RegisteredCourse {
-    private Student student;
-    private Course course;
-    private Index index;
-    private String status;
+	private Student student;
+	private Course course;
+	private Index index;
+	private String status;
 
-    public RegisteredCourse(Student student, Course course, Index index, String status){
-        this.student = student;
-        this.course = course;
-        this.index = index;
-        this.status = status;
-    }
+	public RegisteredCourse(Student student, Course course, Index index, String status) {
+		this.student = student;
+		this.course = course;
+		this.index = index;
+		this.status = status;
+	}
 
-    public Student getStudent(){
-        return this.student;
-    }
-    public void setStudent(Student student){
-        this.student = student;
-    }
+	public Student getStudent() {
+		return student;
+	}
 
-    public Course getCourse(){
-        return this.course;
-    }
-    public void setCourse(Course course){
-        this.course = course;
-    }
+	public void setStudent(Student student) {
+		this.student = student;
+	}
 
-    public Index getIndex(){
-        return this.index;
-    }
-    public void setIndex(Index index){
-        this.index = index;
-    }
+	public void setCourse(Course course) {
+		this.course = course;
+	}
 
-    public String getStatus(){
-        return this.status;
-    }
-    public void setStatus(String status){
-        this.status = status;
-    }
+	public Course getCourse() {
+		return this.course;
+	}
+
+	public void setIndex(Index index) {
+		this.index = index;
+	}
+
+	public Index getIndex() {
+		return null;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
 }
