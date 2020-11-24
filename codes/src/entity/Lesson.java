6199b57e8dee531f939a48e7064f1612eab82525
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 23:59:15 +0800

    Update Lesson.java

--- codes/src/entity/Lesson.java
+++ codes/src/entity/Lesson.java
@@ -1,72 +1,81 @@
 package entity;
 
+import java.io.Serializable;
 import java.util.Calendar;
 
-public class Lesson {
-    private String lessonType;
-    private String lessonGrp;
-    private int lessonDay;
-    private String lessonVenue;
-    private Calendar lessonStart;
-    private Calendar lessonEnd;
-    private String lessonRemarks;
+public class Lesson implements Serializable {
+	private String lessonType;
+	private String lessonGrp;
+	private int lessonDay;
+	private String lessonVenue;
+	private Calendar lessonStart;
+	private Calendar lessonEnd;
+	private String lessonRemarks;
 
-    public Lesson(String lessonType, String lessonGrp, int lessonDay, String lessonVenue, Calendar lessonStart, Calendar lessonEnd, String lessonRemarks){
-        this.lessonType = lessonType;
-        this.lessonGrp = lessonGrp;
-        this.lessonDay = lessonDay;
-        this.lessonVenue = lessonVenue;
-        this.lessonStart = lessonStart;
-        this.lessonEnd = lessonEnd;
-        this.lessonRemarks = lessonRemarks;
-    }
+	public Lesson(String lessonType, String lessonGrp, int lessonDay, String lessonVenue, Calendar lessonStart,
+			Calendar lessonEnd, String lessonRemarks) {
+		this.lessonType = lessonType;
+		this.lessonGrp = lessonGrp;
+		this.lessonDay = lessonDay;
+		this.lessonVenue = lessonVenue;
+		this.lessonStart = lessonStart;
+		this.lessonEnd = lessonEnd;
+		this.lessonRemarks = lessonRemarks;
+	}
 
-    public String getLessonType(){
-        return this.lessonType;
-    }
-    public void setLessonType(String lessonType){
-        this.lessonType = lessonType;
-    }
+	public String getLessonType() {
+		return lessonType;
+	}
 
-    public String getLessonGrp(){
-        return this.lessonGrp;
-    }
-    public void setLessonGrp(String lessonGrp){
-        this.lessonGrp = lessonGrp;
-    }
+	public void setLessonType(String lessonType) {
+		this.lessonType = lessonType;
+	}
 
-    public int getLessonDay(){
-        return this.lessonDay;
-    }
-    public void setLessonDay(int lessonDay){
-        this.lessonDay = lessonDay;
-    }
+	public String getLessonGrp() {
+		return lessonGrp;
+	}
 
-    public String getLessonVenue(){
-        return this.lessonVenue;
-    }
-    public void setLessonVenue(String lessonVenue){
-        this.lessonVenue = lessonVenue;
-    }
+	public void setLessonGrp(String lessonGrp) {
+		this.lessonGrp = lessonGrp;
+	}
 
-    public Calendar getLessonStart(){
-        return this.lessonStart;
-    }
-    public void setLessonStart(Calendar lessonStart){
-        this.lessonStart = lessonStart;
-    }
+	public int getLessonDay() {
+		return lessonDay;
+	}
 
-    public Calendar getLessonEnd(){
-        return this.lessonEnd;
-    }
-    public void setLessonEnd(Calendar lessonEnd){
-        this.lessonEnd = lessonEnd;
-    }
+	public void setLessonDay(int lessonDay) {
+		this.lessonDay = lessonDay;
+	}
 
-    public String getLessonRemarks(){
-        return this.lessonRemarks;
-    }
-    public void setLessonRemarks(String lessonRemarks){
-        this.lessonRemarks = lessonRemarks;
-    }
+	public String getLessonVenue() {
+		return lessonVenue;
+	}
+
+	public void setLessonVenue(String lessonVenue) {
+		this.lessonVenue = lessonVenue;
+	}
+
+	public void setLessonStart(Calendar lessonStart) {
+		this.lessonStart = lessonStart;
+	}
+
+	public Calendar getLessonStart() {
+		return this.lessonStart;
+	}
+
+	public void setLessonEnd(Calendar lessonEnd) {
+		this.lessonEnd = lessonEnd;
+	}
+
+	public Calendar getLessonEnd() {
+		return this.lessonEnd;
+	}
+
+	public String getLessonRemarks() {
+		return lessonRemarks;
+	}
+
+	public void setLessonRemarks(String lessonRemarks) {
+		this.lessonRemarks = lessonRemarks;
+	}
 }
