d250f0afd0cf59502e74699c46c1cd628b3c9b25
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 14:20:38 +0800

    Update AdminMgr.java

--- codes/src/control/AdminMgr.java
+++ codes/src/control/AdminMgr.java
@@ -6,20 +6,22 @@ import java.util.*;
 import entity.Student;
 import entity.RegisteredCourse;
 import entity.Course;
+import entity.Index;
+import entity.Lesson;
 
 
 public class AdminMgr extends CourseMgr{
-    public static void editStudentAccessPeriod(String matricNum, Calendar startTime, Calendar endTime){
+    public static void editStudentAccessPeriod(String matricNum, Calendar startTime, Calendar endTime) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Student> studentList = DataMgr.readStudentList();
         for (Student student: studentList) {
-            if (student.getMatricNum() == matricNum) {
-                student.setStart = startTime;
-                student.setEnd = endTime;
+            if (student.getMatricNum().compareTo(matricNum) == 0) {
+                student.setStart(startTime);
+                student.setEnd(endTime);
             }
         }
     }
 
-    public static void addStudent(String username, String password, String firstName, String lastName, String matricNum, String gender, String nationality, String mobileNum, String email, Calendar startDate, Calendar endDate){
+    public static void addStudent(String username, String password, String firstName, String lastName, String matricNum, char gender, String nationality, int mobileNum, String email, Calendar startDate, Calendar endDate) throws EOFException, ClassNotFoundException, IOException{
         Student newStudent  = new Student(username, password, firstName, lastName, matricNum, gender, nationality, mobileNum, email, startDate, endDate);
         ArrayList<Student> studentList = DataMgr.readStudentList();
         studentList.add(newStudent);
@@ -27,11 +29,11 @@ public class AdminMgr extends CourseMgr{
 
     }
 
-    public static void deleteStudent(String matricNum){
+    public static void deleteStudent(String matricNum) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Student> studentList = DataMgr.readStudentList();
         for (Student student: studentList) {
-            if (student.getMatricNum() == matricNum) {
-                studentList.remove(new Student(student));
+            if (student.getMatricNum().compareTo(matricNum) == 0) {
+                studentList.remove(student);
                 break;
             }
         }
@@ -40,30 +42,28 @@ public class AdminMgr extends CourseMgr{
 
     }
 
-    public static void printStudentListByCourse(String courseCode){
+    public static void printStudentListByCourse(String courseCode) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Student> studentList = DataMgr.readStudentList();
-        ArrayList<Student> newStudentList = null;
         ArrayList<RegisteredCourse> courseList;
         for (Student student: studentList) {
-            courseList = student.getCourseRegistered();
+            courseList = student.getCoursesRegistered();
             for (RegisteredCourse course: courseList)
             {
-                if(course.getCourseCode() == courseCode)
+                if(course.getCourse().getCourseCode().compareTo(courseCode) == 0)
                  System.out.print(student.getFirstName()+" "+student.getLastName()+" "+student.getGender()+" "+student.getNationality());
             }
 
         }
     }
 
-    public static void printStudentListByIndex(String courseCode, int indexNum){
+    public static void printStudentListByIndex(String courseCode, int indexNum) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Student> studentList = DataMgr.readStudentList();
-        ArrayList<Student> newStudentList = null;
         ArrayList<RegisteredCourse> courseList;
         for (Student student: studentList) {
-            courseList = student.getCourseRegistered();
+            courseList = student.getCoursesRegistered();
             for (RegisteredCourse course: courseList)
             {
-                if(course.getCourseCode() == courseCode && course.getIndex() == indexNum)
+                if(course.getCourse().getCourseCode().compareTo(courseCode) == 0 && course.getIndex().getIndexNum() == indexNum)
                     System.out.print(student.getFirstName()+" "+student.getLastName()+" "+student.getGender()+" "+student.getNationality());
             }
 
@@ -71,79 +71,79 @@ public class AdminMgr extends CourseMgr{
 
     }
 
-    public static void addCourse(String courseCode, String courseName, int AU, String school, String courseType, Calendar examDate){
-        ArrayList<Course> courseList = DataMgr.readCourseList();
-        Course newCourse = new Course(courseCode, courseName, AU, school, courseType, examDate);
+    public static void addCourse(String courseCode, String courseName, int AU, String school, String courseType, Calendar examDate) throws EOFException, ClassNotFoundException, IOException{
+    	ArrayList<Course> courseList = DataMgr.readCourseList();
+        Course newCourse = new Course(courseCode, courseName, AU, school, courseType, examDate, new ArrayList<Index>());
         courseList.add(newCourse);
         DataMgr.writeCourseList(courseList);
     }
 
-    public static void updateCourseCode(String oldCourseCode, String newCourseCode){
+    public static void updateCourseCode(String oldCourseCode, String newCourseCode) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == oldCourseCode) {
+            if (course.getCourseCode().compareTo(oldCourseCode) == 0) {
                 course.setCourseCode(newCourseCode);
                 break;
             }
         }
     }
 
-    public static void updateCourseName(String courseCode, String courseName){
+    public static void updateCourseName(String courseCode, String courseName) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
                 course.setCourseName(courseName);
                 break;
             }
         }
     }
 
-    public static void updateAU(String courseCode, int AU){
+    public static void updateAU(String courseCode, int AU) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
                 course.setAU(AU);
                 break;
             }
         }
     }
 
-    public static void updateSchool(String courseCode, String school){
+    public static void updateSchool(String courseCode, String school) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
                 course.setSchool(school);
                 break;
             }
         }
     }
 
-    public static void updateCourseType(String courseCode, String courseType){
+    public static void updateCourseType(String courseCode, String courseType) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
                 course.setCourseType(courseType);
                 break;
             }
         }
     }
 
-    public static void updateExamDate(String courseCode, Calendar examDate){
+    public static void updateExamDate(String courseCode, Calendar examDate) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
                 course.setExamDate(examDate);
                 break;
             }
         }
     }
 
-    public static void addIndex(String courseCode, int indexNum){
-        ArrayList<Course> courseList = DataMgr.readCourseList();
+    public static void addIndex(String courseCode, int indexNum, int vacancy, ArrayList<Lesson> lessonList) throws EOFException, ClassNotFoundException, IOException{
+    	ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
-                ArrayList<Index> indexList= course.getIndexList;
-                Index newIndex = new Index(indexNum);
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
+                ArrayList<Index> indexList= course.getIndexList();
+                Index newIndex = new Index(indexNum, vacancy, new ArrayList<Student>(), lessonList);
                 indexList.add(newIndex);
                 course.setIndexList(indexList);
                 break;
@@ -151,14 +151,14 @@ public class AdminMgr extends CourseMgr{
         }
     }
 
-    public static void deleteIndex(String courseCode, int indexNum){
+    public static void deleteIndex(String courseCode, int indexNum) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
-                ArrayList<Index> indexList= course.getIndexList;
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
+                ArrayList<Index> indexList= course.getIndexList();
                 for(Index index: indexList) {
                     if (index.getIndexNum() == indexNum) {
-                        indexList.remove(new Index(index));
+                        indexList.remove(index);
                         break;
                     }
                 }
@@ -166,11 +166,11 @@ public class AdminMgr extends CourseMgr{
         }
     }
 
-    public static void updateVacancy(String courseCode, int indexNum, int newVacancy){
+    public static void updateVacancy(String courseCode, int indexNum, int newVacancy) throws EOFException, ClassNotFoundException, IOException{
         ArrayList<Course> courseList = DataMgr.readCourseList();
         for (Course course: courseList) {
-            if (course.getCourseCode() == courseCode) {
-                ArrayList<Index> indexList= course.getIndexList;
+            if (course.getCourseCode().compareTo(courseCode) == 0) {
+                ArrayList<Index> indexList= course.getIndexList();
                 for(Index index: indexList) {
                     if (index.getIndexNum() == indexNum) {
                         index.setVacancy(newVacancy);
@@ -181,10 +181,10 @@ public class AdminMgr extends CourseMgr{
         }
     }
 
-    public static boolean validateMatric(String matricNum) {
+    public static boolean validateMatric(String matricNum) throws EOFException, ClassNotFoundException, IOException {
         ArrayList<Student> studentList = DataMgr.readStudentList();
         for (Student student: studentList) {
-            if (student.getMatricNum() == matricNum)
+            if (student.getMatricNum().compareTo(matricNum) == 0)
                 return true;
         }
         return false;
