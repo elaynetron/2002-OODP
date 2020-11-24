aa1cc4badd901493307eef3a8c68574c1c4932e7
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 23:58:40 +0800

    Update Course.java

--- codes/src/entity/Course.java
+++ codes/src/entity/Course.java
@@ -1,73 +1,81 @@
 package entity;
 
-import java.util.Calendar;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
-public class Course {
-    private String courseCode;
-    private String courseName;
-    private int AU;
-    private String school;
-    private String courseType;
-    private Calendar examDate;
-    private ArrayList<Index> indexList;
+public class Course implements Serializable {
+	String courseCode;
+	String courseName;
+	int AU;
+	String school;
+	String courseType;
+	Calendar examDate;
+	ArrayList<Index> indexList;
 
-    public Course(String courseCode, String courseName, int AU, String school, String courseType, Calendar examDate, ArrayList<Index> indexList){
-        this.courseCode = courseCode;
-        this.courseName = courseName;
-        this.AU = AU;
-        this.school = school;
-        this.courseType = courseType;
-        this.examDate = examDate;
-        this.indexList = indexList;
-    }
+	public Course(String courseCode, String courseName, int AU, String school, String courseType, Calendar examDate,
+			ArrayList<Index> indexList) {
+		this.courseCode = courseCode;
+		this.courseName = courseName;
+		this.AU = AU;
+		this.school = school;
+		this.courseType = courseType;
+		this.examDate = examDate;
+		this.indexList = indexList;
+	}
 
-    public String getCourseCode(){
-        return this.courseCode;
-    }
-    public void setCourseCode(String courseCode){
-        this.courseCode = courseCode;
-    }
+	public String getCourseCode() {
+		return courseCode;
+	}
 
-    public String getCourseName(){
-        return this.courseName;
-    }
-    public void setCourseName(String courseName){
-        this.courseName = courseName;
-    }
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
 
-    public int getAU(){
-        return this.AU;
-    }
-    public void setAU(int AU){
-        this.AU = AU;
-    }
+	public String getCourseName() {
+		return courseName;
+	}
 
-    public String getSchool(){
-        return this.school;
-    }
-    public void setSchool(String school){
-        this.school = school;
-    }
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
 
-    public String getCourseType(){
-        return this.courseType;
-    }
-    public void setCourseType(String courseType){
-        this.courseType = courseType;
-    }
+	public int getAU() {
+		return AU;
+	}
 
-    public Calendar getExamDate(){
-        return this.examDate;
-    }
-    public void setExamDate(Calendar examDate){
-        this.examDate = examDate;
-    }
+	public void setAU(int AU) {
+		this.AU = AU;
+	}
 
-    public ArrayList<Index> getIndexList(){
-        return this.indexList;
-    }
-    public void setIndexList(ArrayList<Index> indexList){
-        this.indexList = indexList;
-    }
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public String getCourseType() {
+		return courseType;
+	}
+
+	public void setCourseType(String courseType) {
+		this.courseType = courseType;
+	}
+
+	public Calendar getExamDate() {
+		return examDate;
+	}
+
+	public void setExamDate(Calendar examDate) {
+		this.examDate = examDate;
+	}
+
+	public ArrayList<Index> getIndexList() {
+		return indexList;
+	}
+
+	public void setIndexList(ArrayList<Index> indexList) {
+		this.indexList = indexList;
+	}
 }
