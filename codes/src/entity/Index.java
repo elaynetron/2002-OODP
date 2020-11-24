44ca320084a54f9698be35194c196db788f086da
elaynetron <56335018+elaynetron@users.noreply.github.com>
2020-11-24 23:58:59 +0800

    Update Index.java

--- codes/src/entity/Index.java
+++ codes/src/entity/Index.java
@@ -1,45 +1,50 @@
 package entity;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Index {
+public class Index implements Serializable{
     private int indexNum;
     private int vacancy;
     private ArrayList<Student> waitList;
     private ArrayList<Lesson> lessonList;
 
-    public Index(int indexNum, int vacancy, ArrayList<Student> waitList, ArrayList<Lesson> lessonList){
+    public Index(int indexNum, int vacancy, ArrayList<Student> waitList, ArrayList<Lesson> lessonList) {
         this.indexNum = indexNum;
         this.vacancy = vacancy;
         this.waitList = waitList;
         this.lessonList = lessonList;
     }
 
-    public int getIndexNum(){
-        return this.indexNum;
+	public int getIndexNum() {
+        return indexNum;
     }
-    public void setIndexNum(int indexNum){
+
+    public void setIndexNum(int indexNum) {
         this.indexNum = indexNum;
     }
 
-    public int getVacancy(){
-        return this.vacancy;
+    public int getVacancy() {
+        return vacancy;
     }
-    public void setVacancy(int vacancy){
+
+    public void setVacancy(int vacancy) {
         this.vacancy = vacancy;
     }
 
-    public ArrayList<Student> getWaitList(){
-        return this.waitList;
+    public ArrayList<Student> getWaitList() {
+        return waitList;
     }
-    public void setWaitList(ArrayList<Student> waitList){
+
+    public void setWaitList(ArrayList<Student> waitList) {
         this.waitList = waitList;
     }
 
-    public ArrayList<Lesson> getLessonList(){
-        return this.lessonList;
+    public ArrayList<Lesson> getLessonList() {
+        return lessonList;
     }
-    public void setLessonList(ArrayList<Lesson> lessonList){
+
+    public void setLessonList(ArrayList<Lesson> lessonList) {
         this.lessonList = lessonList;
     }
 }
